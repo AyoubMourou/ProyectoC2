39b40bd7920c5a7349d167063766f6aaa69baf7f
AyoubMourou <99480137+AyoubMourou@users.noreply.github.com>
2022-03-01 13:58:37 +0100

    Add files via upload

--- ProyectoWeb/functions.js
+++ ProyectoWeb/functions.js
@@ -5,7 +5,6 @@ function comprobarDatosIntroducidos() {
 
     if ($('#correo').val() != correo || $('#contra').val() != contra) {
         document.getElementById("errorDatos").style.display = "block";
-        alert($('#correo').val());
     } else {
         setCookie(nomCookie, correo, 30);
         VolverPagPrincipal();
@@ -37,24 +36,28 @@ function getCookie(cname) {
 
 function RatonEncima(c){
     
-
     c.style.maxWidth= 425+"px";
     c.style.maxHeight=425+"px";
     
 }
+
 function RatonFuera(c){
     c.style.maxWidth=400+"px";
     c.style.maxHeight=400+"px";
 }
+
 function EntrarEn_C() {
     location.replace("pag_C.html")
 }
+
 function VolverPagPrincipal(){
     location.replace("pag_principal.html")
 }
+
 window.onload=function(){
     $('.contenidoSesion').hide()
 }
+
 function HideShowSesion(value){
     var sesion=$('.contenidoSesion').eq(value)
     sesion.toggle()
@@ -67,3 +70,41 @@ function HideShowSesion(value){
         $(".iconoFlecha").eq(value).toggleClass("bi bi-chevron-up")
 }
 
+function AbrirForm(){
+    $('#modal1').show();
+};
+
+function cerrarForm() {
+    $('#modal1').hide(); // Now the pop up is hided.
+}
+
+function comprobarClave(){
+
+    var clave1 = $('#password').val();
+    var clave2 = $('#repetir-password').val();
+    var correo = $('#email').val();
+    
+    var condicionesContra = /^(?=.*\d)(?=.*[a-záéíóúüñ])/;
+
+    if (correo == '' || clave2 == '' || clave1 == '') {
+        alert("Rellena los campos")
+    }
+    else
+        if (!validarEmail(correo)) {
+            alert("el correo es incorrecto")
+        } else if (!condicionesContra.test(clave1) || clave1.length < 6) {
+                alert("la contraseña debe contener al menos un numero y una letra. Tiene que tener 6 caracteres")
+            } else if (clave1 == clave2) {
+                location.replace("loginPage.html")
+                } else 
+                    alert("las contraseñas no coinciden")
+
+}
+
+function validarEmail(valor) {
+    if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
+        return true;
+    } else {
+      return false;
+    }
+  }
