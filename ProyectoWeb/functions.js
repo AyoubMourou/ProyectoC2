7c4b1b9fa5d77d5f41ddac7162b55140b8191029
AyoubMourou <99480137+AyoubMourou@users.noreply.github.com>
2022-03-02 09:50:57 +0100

    Add files via upload

--- ProyectoWeb/functions.js
+++ ProyectoWeb/functions.js
@@ -1,3 +1,4 @@
+// comprueba que el usuario ha introducido bien los datos para logearse
 function comprobarDatosIntroducidos() {
     var correo = "esplai22@gmail.com";
     var contra = "esplai22";
@@ -11,6 +12,7 @@ function comprobarDatosIntroducidos() {
     }
 }
 
+// guardamos la cookie
 function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -18,6 +20,7 @@ function setCookie(cname, cvalue, exdays) {
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+// funcion por si queremos recuperar la cookie
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -46,18 +49,22 @@ function RatonFuera(c){
     c.style.maxHeight=400+"px";
 }
 
+// cambiar a pagina C1
 function EntrarEn_C() {
     location.replace("pag_C.html")
 }
 
+// cambiar a pagina principal
 function VolverPagPrincipal(){
     location.replace("pag_principal.html")
 }
+
 
 window.onload=function(){
     $('.contenidoSesion').hide()
 }
 
+// mostrar y ocultar nombres de los temas
 function HideShowSesion(value){
     var sesion=$('.contenidoSesion').eq(value)
     sesion.toggle()
@@ -70,23 +77,28 @@ function HideShowSesion(value){
         $(".iconoFlecha").eq(value).toggleClass("bi bi-chevron-up")
 }
 
+// abrir formulario de peticion
 function AbrirForm(){
     $('#modal1').show();
 };
 
+// cerrar formulario de peticion
 function cerrarForm() {
     $('#modal1').hide(); // Now the pop up is hided.
 }
 
+// comprobar que los datos del registro son correctos
 function comprobarClave(){
 
     var clave1 = $('#password').val();
     var clave2 = $('#repetir-password').val();
+    var nombre = $('#nombre').val();
+    var apellido = $('#apellidos').val();
     var correo = $('#email').val();
     
     var condicionesContra = /^(?=.*\d)(?=.*[a-záéíóúüñ])/;
 
-    if (correo == '' || clave2 == '' || clave1 == '') {
+    if (correo == '' || clave2 == '' || clave1 == '' || nombre == '' || apellido == '') {
         alert("Rellena los campos")
     }
     else
@@ -101,10 +113,16 @@ function comprobarClave(){
 
 }
 
+// validacion del formato del correo
 function validarEmail(valor) {
     if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(valor)){
         return true;
     } else {
       return false;
     }
-  }
+}
+
+// funcion para salir de la pagina principal
+function logOut() {
+    location.replace("loginPage.html");
+}
